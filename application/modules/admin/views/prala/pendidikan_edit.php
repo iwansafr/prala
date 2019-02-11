20a88ce9114a63f7c3bebf073e3cc5bc0abde7ff
iwansafr <iwansafr@gmail.com>
2019-02-11 18:47:47 +0700

    database and status pendidikan list

--- application/modules/admin/views/prala/pendidikan_edit.php
+++ application/modules/admin/views/prala/pendidikan_edit.php
@@ -5,7 +5,7 @@
 				<div class="form-group">
 					<label for="">Cari Peserta</label>
 					<div class="form-inline">
-						<input type="text" name="reg_id" class="form-control" placeholder="no registration">
+						<input type="text" name="reg_id" class="form-control" value="<?php echo @$_GET['reg_id'] ?>" placeholder="no registration">
 						<input type="submit" class="btn btn-default" value="cari">
 					</div>
 				</div>
@@ -26,14 +26,18 @@ if(!empty($_GET['reg_id']))
 		</div>
 		<div class="panel panel-body">
 			<?php 
-			$show = array('nama','tempat_lahir','tgl_lahir');
+			$show = array('kode_pelaut','nama','tempat_lahir','tgl_lahir','kelamin');
 			foreach ($data as $key => $value) 
 			{
 				if(in_array($key, $show))
 				{
+					if($key == 'kelamin'){
+						$kelamin = array('Perempuan','Laki-laki');
+						$value = $kelamin[$value];
+					}
 					?>
 					<div class="form-group ">
-						<label for="<?php echo $key ?>"><?php echo $key ?></label>
+						<label for="<?php echo str_replace('_',' ',$key) ?>"><?php echo str_replace('_',' ',$key) ?></label>
 						<input type="text" value="<?php echo $value ?>" class="form-control" readonly>
 					</div>
 					<?php
@@ -48,8 +52,28 @@ if(!empty($_GET['reg_id']))
 	$form = new zea();
 	$form->init('edit');
 	$form->setTable('prala_pendidikan');
+	$form->setHeading('Tambah Status Pendidikan');
+	$form->setEditStatus(FALSE);
 	$form->setId(@intval($_GET['id']));
 	$form->addInput('prodi_id','dropdown');
+	$form->setLabel('prodi_id','Program Studi');
 	$form->tableOptions('prodi_id','prodi','id','title');
+
+	$form->addInput('nama_kapal','text');
+	$form->setLabel('nama_kapal','Nama Kapal');
+	$form->addInput('nama_perusahaan','text');
+	$form->setLabel('nama_perusahaan','Nama Perusahaan');
+	$form->addInput('prala_id','hidden');
+	$form->setValue('prala_id',$data['id']);
+	$form_tgl = array('masuk','ujian_nasional','ujian_pra_prala','sign_on','sign_off','ujian_trb','ujian_paska','wisudha');
+
+	foreach ($form_tgl as $key => $value) 
+	{
+		$form->addInput($value,'text');
+		$form->setType($value,'date');
+		$form->setLabel($value, str_replace('_', ' ', $value));
+	}
+
+
 	$form->form();
 }
--- application/modules/admin/views/prala/pendidikan_list.php
+++ application/modules/admin/views/prala/pendidikan_list.php
@@ -1,2 +1,13 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
+$form = new zea();
+$form->init('roll');
+$form->setTable('prala_pendidikan');
+$form->search();
+$form->join('prala','ON(prala.id=prala_pendidikan.prala_id)','prala_pendidikan.id,prala.nama,prala_pendidikan.nama_kapal');
+// $form->group_by('prala.id');
+$form->addInput('id','plaintext');
+$form->addInput('nama','plaintext');
+$form->addInput('nama_kapal','plaintext');
+$form->form();
+pr($form->getData());
