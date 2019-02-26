90e098bd20f9983de08d7c1bab3d1dc1ae360e8f
iwansafr <iwansafr@gmail.com>
2019-02-26 20:14:21 +0700

    header on side logo

--- application/modules/home/views/templates/academy/logo.php
+++ application/modules/home/views/templates/academy/logo.php
@@ -1,7 +1,14 @@
 <div class="col-12 h-100">
 	<div class="header-content h-100 d-flex align-items-center justify-content-between">
 		<div class="academy-logo">
-			<a href="<?php echo base_url()?>"><img src="<?php echo image_module('config/logo', @$logo['image']) ?>" alt="" width="<?php echo @$logo['width'] ?>" height="<?php echo @$logo['height'] ?>"></a>
+			<div class="form-inline">
+				<a href="<?php echo base_url()?>"><img src="<?php echo image_module('config/logo', @$logo['image']) ?>" alt="" width="<?php echo @$logo['width'] ?>" height="<?php echo @$logo['height'] ?>"></a>
+				<div class="header d-none d-md-block d-lg-block d-xl-block">
+					<h4><?php echo @$meta['contact']['name'] ?></h4>
+					Alamat : <?php echo @$meta['contact']['address'] ?>
+					Telepon : <?php echo @$meta['contact']['phone'] ?>
+				</div>
+			</div>
 		</div>
 		<div class="login-content">
 			<a href="<?php echo base_url()?>">Register / Login</a>
